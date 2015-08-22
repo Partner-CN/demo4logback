e92f291bbfcd1ffbeab6f88152426c49407eea64
qiuyinglanshan <154197116@qq.com>
2015-08-22 22:56:37 +0800

    test from my eclipse

--- src/main/java/cn/partner/joe/demo/logback/HelloWorld.java
+++ src/main/java/cn/partner/joe/demo/logback/HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld {
 			@Override
 			public void run() {
 				for (;;) {
-					log.info("hello,world");
+					log.info("why are you so busy ?");
 					try {
 						Thread.sleep(1000);
 					} catch (InterruptedException e) {
