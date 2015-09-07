e48668d2a99f7cf776da1760a3c3e5ac9459be8f
Yonsin Joe <qiao.yongxin@navercorp.com>
2015-09-07 15:28:54 +0800

    sdfsaf

--- src/main/java/cn/partner/joe/demo/logback/HelloWorld.java
+++ src/main/java/cn/partner/joe/demo/logback/HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld {
 			@Override
 			public void run() {
 				for (;;) {
-					log.info("why are you so busy ?433434^5thga");
+					log.info("why are you so busy ?433434");
 					try {
 						Thread.sleep(1000);
 					} catch (InterruptedException e) {
